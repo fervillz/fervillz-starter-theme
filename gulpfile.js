e629ba62aaeeb5716d55a1807e482db319fe6819
fernando villamor jr <fervillz@gmail.com>
2016-12-01 14:28:26 -0800

    updated gulpfile and package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 'use strict';
- 
+
+//Load the plugins 
 var gulp = require('gulp'),
 	sass = require('gulp-sass');
 
+// compile scss and minify.
+// this can run from Terminal with $ gulp sass.
 gulp.task('sass', function () {
   return gulp.src('assets/sass/**/*.scss')
+  	//We use .pipe() to pipe the source file(s) to a plugin.
+    //compile the scss
     .pipe(sass.sync().on('error', sass.logError))
+    //output it in 
     .pipe(gulp.dest('assets/css/'));
 });
 
