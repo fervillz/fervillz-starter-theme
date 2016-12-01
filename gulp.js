3d85c467c5db3c812b11c5fb28ca33447134ad0e
fernando villamor jr <fervillz@gmail.com>
2016-12-01 13:05:18 -0800

    update gulp file and style.css

--- gulp.js
+++ gulp.js
@@ -1,4 +1,13 @@
+'use strict';
+ 
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  return gulp.src('assets/sass/**/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('assets/css'));
+});
 
 gulp.task('default', function() {
   // place code for your default task here
