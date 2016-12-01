e1e0d7b3e00e0d274dd819f686d802395b6efc30
fernando villamor jr <fervillz@gmail.com>
2016-12-01 13:36:23 -0800

    update fulp

--- gulp.js
+++ gulp.js
@@ -6,7 +6,7 @@ var sass = require('gulp-sass');
 gulp.task('sass', function () {
   return gulp.src('assets/sass/**/*.scss')
     .pipe(sass.sync().on('error', sass.logError))
-    .pipe(gulp.dest('assets/css'));
+    .pipe(gulp.dest('assets/css/'));
 });
 
 // What will be run with simply writing "$ gulp"
