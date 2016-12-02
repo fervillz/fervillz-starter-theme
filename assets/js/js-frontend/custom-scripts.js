264831c3cd414647c45d02348a1b437cacef7087
fernando villamor jr <fervillz@gmail.com>
2016-12-01 19:37:40 -0800

    activate headroom.js functionality

--- assets/js/js-frontend/custom-scripts.js
+++ assets/js/js-frontend/custom-scripts.js
@@ -1,6 +1,25 @@
-/**
- * File custom-scripts.js.
- *
- *
- * Contains the custom jscripts 
- */
+jQuery( document ).ready( function ( $ ) {
+	
+	// Initialise the scroll animation plugin.
+	// See it's bundle at scripts/vendor/aos.min.js
+	AOS.init({
+		duration: 600,
+		delay: 100
+	});
+
+	// grab an element
+	var myHeader = document.querySelector("#masthead");
+	
+	// construct an instance of Headroom, passing the element
+	var headroom  = new Headroom(myHeader, {
+	  "offset": 90,
+	  tolerance : {
+			up : 5,
+			down : 0
+		}
+	});
+
+	// initialise
+	headroom.init(); 
+
+});
